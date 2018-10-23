1e10a55530e4435339096f2c30c40bd92361e58e
TBY\User <tby1994@gmail.com>
2018-10-23 23:05:20 +0800

    Updated ml code for pi

--- Machine Learning/Pi/Prediction.py
+++ Machine Learning/Pi/Prediction.py
@@ -1,14 +1,8 @@
 
 # coding: utf-8
 
-# In[80]:
+# In[50]:
 
-
-# scale the data points
-from sklearn.preprocessing import StandardScaler 
-
-# Neural Network model
-from sklearn.neural_network import MLPClassifier
 
 # Importation of  model
 import pickle
@@ -16,19 +10,13 @@ import pickle
 # data analysis
 import pandas as pd
 
-# scale the data points
-from sklearn.preprocessing import StandardScaler 
-
-import csv
-
 def predict():
-
     filename_mlp = "Neural_Network_Model.sav"
     filename_svm = "SVM.sav"
     filename_rd = "RD.sav"
-#     data = pd.read_csv(r'/home/pi/3002/main/test_dataset.csv')
-    data = pd.read_csv(r'C:\Users\User\Desktop\Study\Year 3\Sem 1\CG3002\DanceProject\Machine Learning\extracted data\test_dataset.csv')
-    
+#     data = pd.read_csv(r'/home/pi/Desktop/test_dataset.csv')
+#     data = pd.read_csv(r'C:\Users\User\Desktop\Study\Year 3\Sem 1\CG3002\DanceProject\Machine Learning\extracted data\test_dataset.csv')
+    data = pd.read_csv(r'C:\Users\User\Desktop\chicken_extracted_dataset.csv')
     mlp_model = pickle.load(open(filename_mlp, 'rb'))
     svm_model = pickle.load(open(filename_svm, 'rb'))
     rd_model = pickle.load(open(filename_rd, 'rb'))
