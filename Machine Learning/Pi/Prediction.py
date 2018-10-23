7751d2d197dabcd07b6e9344d007c7b27d3f9191
Infinity-Ace <zechariah.tan.jl@gmail.com>
2018-10-23 15:59:07 +0800

    Update Prediction.py

--- Machine Learning/Pi/Prediction.py
+++ Machine Learning/Pi/Prediction.py
@@ -16,35 +16,24 @@ import pickle
 # data analysis
 import pandas as pd
 
-# scale the data points
-from sklearn.preprocessing import StandardScaler 
-
-import csv
-
 def predict()
 
     filename_mlp = "Neural_Network_Model.sav"
     filename_rd = "Random_Forestry_Model.sav"
-    data = pd.read_csv(r'/home/pi/Desktop/cleaned_dataset.csv')
+    data = pd.read_csv(r'/home/pi/Desktop/extracted_dataset.csv')
     
     mlp_model = pickle.load(open(filenameMLP, 'rb'))
     rd_model = pickle.load(open(filenameRD, 'rb'))
     
     prediction_rd = rd_model.predict(data)
+
+    prediction_mlp = mlp_model.predict(data)
     
-    # data scaling to improve accuracy of mlp
-    scaler = StandardScaler()
-    scaler.fit(data)
-    
-    prediction_mlp = mlp_model.predict(scaler.transform(data))
-    
-    predictions = prediction_mlp + prediction_rd
+    predictions = prediction_rd.tolist() + prediction_mlp.tolist()
     
     mapping = { 1 : 'chicken', 2 : 'number7', 3 : 'sidestep', 4 : 'turnclap', 5 : 'wiper'}
     output = pd.DataFrame({'activity' : predictions})
     output = output.replace({'activity': mapping})   
-    b = output.activity.mode()
-    myFile = open('output.csv', 'w')  
-    with myFile:  
-        myFile.write(b[0])
+    return output.activity.mode()
+
 
